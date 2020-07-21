04ee130f3da4bf39eba40555711671e11b84e3d9
carwbrown <carwbrown@google.com>
2020-07-21 00:06:01 -0400

    add comment

--- public/workers/appleCount.js
+++ public/workers/appleCount.js
@@ -1,3 +1,5 @@
+// based off of tutorial from https://dev.to/daviddalbusco/react-and-web-workers-4io3
+
 self.onmessage = async ($event) => {
   if ($event && $event.data && $event.data.msg === "incApple") {
     const newCounter = incApple($event.data.countApple);
